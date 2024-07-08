4425102e3c22ca27597ce077a3155c73cca57b1a
alexcoss <kozachenko.aleksander@gmail.com>
2024-07-08 14:49:17 +0200

    security keycloak

--- src/main/java/dev/alexcoss/carservice/config/SecurityConfig.java
+++ src/main/java/dev/alexcoss/carservice/config/SecurityConfig.java
@@ -1,21 +1,38 @@
 package dev.alexcoss.carservice.config;
 
+import dev.alexcoss.carservice.config.security.JwtConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtConverter jwtConverter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().permitAll())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/**").authenticated()
+                .requestMatchers(HttpMethod.PATCH, "/**").authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/**").hasRole("admin")
+                .anyRequest().authenticated())
+
+            .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
             .build();
     }
 }
